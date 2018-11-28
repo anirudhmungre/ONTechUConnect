748980984cae5a853aa08aeb66ccf95122c34973
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-28 18:35:29 -0500

    updating stuff

--- ontechu-connect/src/views/Enrolled.vue
+++ ontechu-connect/src/views/Enrolled.vue
@@ -6,40 +6,13 @@
                     <v-flex>
                         <v-card class="mb-3">
                             <v-toolbar dark color="accent">
-                                <v-toolbar-title class="text-uppercase">Are you okay?</v-toolbar-title>
+                                <v-toolbar-title class="text-uppercase">Enrolled Courses</v-toolbar-title>
                                 <v-spacer></v-spacer>
-                                <v-btn icon @click="ShowHelp = !ShowHelp">
-                                    <v-icon>{{ ShowHelp ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                                </v-btn>
                             </v-toolbar>
                             <v-slide-y-transition>
-                                <v-card-text v-show="ShowHelp">
+                                <v-card-text>
                                     <v-layout row>
-                                        <v-flex xs2>
-                                            <v-container fill-height>
-                                                <v-layout justify-center align-center>
-                                                    <v-flex shrink>
-                                                        <v-card flat class="text-xs-center">
-                                                            <v-icon size="64" color="primary">announcement</v-icon>
-                                                        </v-card>
-                                                    </v-flex>
-                                                </v-layout>
-                                            </v-container>
-                                        </v-flex>
-                                        <v-flex xs10>
-                                            <v-card flat>
-                                                <v-card-text>
-                                                    The university has many resources available to help you when you are
-                                                    not OK.
-                                                </v-card-text>
-                                                <v-card-actions>
-                                                    <v-spacer></v-spacer>
-                                                    <v-btn href="https://uoit.ca/forms/areyouokay/index.php"
-                                                           target="_blank" outline color="accent">HELP
-                                                    </v-btn>
-                                                </v-card-actions>
-                                            </v-card>
-                                        </v-flex>
+                                        HELLO
                                     </v-layout>
                                 </v-card-text>
                             </v-slide-y-transition>
