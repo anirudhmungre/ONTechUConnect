240518634f94c261d6c923aa83951c3583b50eb7
Matthew Boivin <31860965+MattyB97@users.noreply.github.com>
2018-11-28 11:10:30 -0500

    Added active schools api

--- server/routes/course.js
+++ server/routes/course.js
@@ -106,8 +106,35 @@ router.post('/admin/school/ontechu/course/total', (req, res) => {
 })
 
 
-router.post('/admin/active/schools', (req, res) => {
-
+router.post('/admin/school/active', (req, res) => {
+    let post = {
+        admin: req.body.admin,
+    }
+    sql.query("SELECT * FROM activeSchools",
+        (results, fields) => {
+            if (admin){
+                return res.json(resp.make()
+                    .setMessage("Query successful Admin!")
+                    .setResponseCode(200)
+                    .setData(results)
+                )
+            }
+            else{
+                return res.json(resp.make()
+                    .setMessage("Query successful Student!")
+                    .setResponseCode(200)
+                    .setData()
+                )
+            }
+        }, (error) => {
+            if (error) {
+                return res.json(resp.make()
+                    .setError(error)
+                    .setResponseCode(500)
+                    .setMessage("There was an error :(")
+                )
+            }
+    })
 })
 
 router.post('/admin/allstudents', (req, res) => {
