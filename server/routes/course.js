ce5a4bb7a834622ad8ae9f854e42c590153415f0
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-28 22:49:09 -0500

    all courses

--- server/routes/course.js
+++ server/routes/course.js
@@ -6,6 +6,32 @@ let router = express.Router()
 let sql = new OurSQL()
 let con = sql.getConnection()
 
+router.post('/all', (req, res) => {
+    let post = {
+        scID: req.body.scid,
+        depName: req.body.depName
+    }
+    sql.query(`SELECT C.* FROM Courses AS C, Department AS D, Faculty AS F WHERE C.departmentID=D.dID AND D.facultyID=F.fID AND D.name=${con.escape(post.depName)} AND F.schoolID=${con.escape(post.scID)}`,
+        (results, fields) => {
+                return res.json(resp.make()
+                    .setMessage("Query successful Student!")
+                    .setResponseCode(200)
+                    .setData(results)
+                )
+        }, (error) => {
+            if (error) {
+                return res.json(resp.make()
+                    .setError(error)
+                    .setResponseCode(500)
+                    .setMessage("There was an error :(")
+                )
+            }
+    })
+})
+
+// Enrolled courses of a student
+// Front end sends the student ID (of logged in student) 
+// and it returns all enrolled courses of the student
 router.post('/student/ecourses', (req, res) => {
     let post = {
         sID: req.body.sid 
@@ -28,6 +54,7 @@ router.post('/student/ecourses', (req, res) => {
     })
 })
 
+// All instructors in a specific school ID defined by the admin
 router.post('/admin/instructors', (req, res) => {
     let post = {
         admin: req.body.admin,
@@ -59,7 +86,8 @@ router.post('/admin/instructors', (req, res) => {
     })
 })
 
-
+// All courses in a department of a school defined by the students school
+// The student knows its school because it is sent it after authentication
 router.post('/student/depcourses', (req, res) => {
     let post = {
         scID: req.body.scid,
@@ -83,7 +111,7 @@ router.post('/student/depcourses', (req, res) => {
     })
 })
 
-
+// The total students per course at ontechu
 router.post('/admin/school/ontechu/course/total', (req, res) => {
     let post = {
         admin: req.body.admin,
@@ -114,7 +142,7 @@ router.post('/admin/school/ontechu/course/total', (req, res) => {
     })
 })
 
-
+// All courses in the database that have students enrolled in it
 router.post('/admin/school/active', (req, res) => {
     let post = {
         admin: req.body.admin
@@ -145,7 +173,7 @@ router.post('/admin/school/active', (req, res) => {
     })
 })
 
-
+// All students in the database
 router.post('/admin/allstudents', (req, res) => {
     let post = {
         admin: req.body.admin,
@@ -176,7 +204,7 @@ router.post('/admin/allstudents', (req, res) => {
     })
 })
 
-
+// 
 router.post('/student/activecourses', (req, res) => {
     let post = {
         admin: req.body.admin,
